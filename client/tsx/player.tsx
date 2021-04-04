026c6bf47a8a71f179e911bdfcc2612d1f29fe03
Paul Boon <gvr37.p.l.boon@gmail.com>
2021-04-04 18:55:15 +0200

    p

--- client/tsx/player.tsx
+++ client/tsx/player.tsx
@@ -7,17 +7,9 @@ function RenderPlayer(props:{player:Player, client:Client, onClick?}){
         bordercolor = 'red'
     }
 
-    
-    var highlightcolor = 'black'
-    if(props.player.sessionid == props.client.sessionid){
-        highlightcolor = 'blue'
-    }
-
     var cardchildren = props.player._children(e => true)
-    return (<div onClick={props.onClick} className="player" style={{margin:'10px', border:`3px solid ${highlightcolor}`}}>
-        <div style={{padding:'10px', border:`1px solid ${bordercolor}`}}>
-            <div>{props.player.name} {sessionplayer.id == props.player.id ? "(yourself)" : ""} {props.player.disconnected ? "(disconnected)" : ""}</div>
-            <div>{cardchildren.length}</div>
-        </div>
+    return (<div style={{margin:'10px', padding:'10px', border:`1px solid ${bordercolor}`}}>
+        <div>{props.player.name} {sessionplayer.id == props.player.id ? "(yourself)" : ""} {props.player.disconnected ? "(disconnected)" : ""}</div>
+        <div>{cardchildren.length}</div>
     </div>)
 }
